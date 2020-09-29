6f284cffb4193c2a47185d532d4854f5f4c10621
RevenantBane <kenbob.21@gmail.com>
2020-09-29 02:33:20 -0700

    added audio thrusting sound fixed movement controls

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -6,21 +6,37 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rgbody;
     public float rotationSpeed = 90;
-
+    AudioSource RocketThrust;
+    
     // Start is called before the first frame update
     void Start() {
         rgbody = GetComponent<Rigidbody>();
+        RocketThrust = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
-    private void ProcessInput(){
+
+    private void Thrust(){
         if(Input.GetKey(KeyCode.Space)){
             rgbody.AddRelativeForce(Vector3.up);
             print("Thrusting");
-        } 
+
+        if(!RocketThrust.isPlaying){
+            RocketThrust.Play();
+            } 
+        } else {
+            RocketThrust.Stop();
+        }
+    }
+    
+    private void Rotate(){
+
+        rgbody.freezeRotation = true; //take manual control of rotation
+
         if (Input.GetKey(KeyCode.A)){
             transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
             print("rotate left");
@@ -29,5 +45,7 @@ public class Rocket : MonoBehaviour {
             transform.Rotate(-Vector3.forward * Time.deltaTime * rotationSpeed);
             print("rotate right");
         }
+
+        rgbody.freezeRotation = false; //resume physics control of rotation
     }
 }
