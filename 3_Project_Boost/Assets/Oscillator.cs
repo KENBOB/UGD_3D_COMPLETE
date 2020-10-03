139c74ca0c17819ccad28fa0c8a9994e46bf3938
RevenantBane <kenbob.21@gmail.com>
2020-10-03 15:26:17 -0700

    Designed 2 level scenes and 1 test scene added sound animations to the rocket added particle animations to the rocket started script for wall object movement

---
+++ 3_Project_Boost/Assets/Oscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attribute that stops the object from obtaining multiple scripts
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    Vector3 startingPos; //must be stored for absolute movement
+
+    //range attribute to shows slider in inspector
+    [Range(0, 1)] [SerializeField] float movementFactor; //0 for not moved, 1 for fully moved.
+    // Start is called before the first frame update
+    void Start()
+    {
+        //stored starting position
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,58 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
-    Rigidbody rgbody;
-    AudioSource RocketThrust;
+    public Rigidbody rgbody;
+    public AudioSource CurrentAudioSound;
     public float rotationSpeed = 100f;
     public float mainThrust = 100f;
-    //serialized field can change in inspector, but not from other scripts
+    public AudioClip MainEngine;
+    public AudioClip LevelSuccess;
+    public AudioClip DeathClip;
+
+    public ParticleSystem MainEngineAnimation;
+    public ParticleSystem LevelCompleteAnimation;
+    public ParticleSystem DeathAnimation;
+    //[SerializeField] <- syntax serialized field can change in inspector, but not from other scripts
     //public can change from inspector and other scripts
+    enum State {Alive, Dying, Transcending};
+    [SerializeField] State CurrentStatus = State.Alive;
     
     // Start is called before the first frame update
     void Start() {
         rgbody = GetComponent<Rigidbody>();
-        RocketThrust = GetComponent<AudioSource>();
+        CurrentAudioSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
-        Thrust();
-        Rotate();
+        if(CurrentStatus == State.Alive){
+            ThrustInputResponse();
+            RotateInputResponse();
+        }
     }
 
+    //Collision detection for changing states
     void OnCollisionEnter(Collision collision){
         print("Collided");
+        // ignore collisions when dead
+        if (CurrentStatus != State.Alive) {
+            return;
+        } 
+        
         switch(collision.gameObject.tag){
             case "Friendly":
                 //do nothing
                 print("OK");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                StartSuccessSequence();
                 break;
             default:
-                print("dead");
+                StartDeathSequence();
                 break;
         }
     }
 
-    private void Thrust(){
-        if(Input.GetKey(KeyCode.Space)){
-            rgbody.AddRelativeForce(Vector3.up * mainThrust);
-            print("Thrusting");
+    //State Changes
+    private void StartSuccessSequence(){
+        print("Finish");
+        CurrentStatus = State.Transcending;
+        CurrentAudioSound.Stop();
+        CurrentAudioSound.PlayOneShot(LevelSuccess);
+        LevelCompleteAnimation.Play();
+        //invoke to load string to call load next scene after 1 second
+        Invoke("LoadNextLevel", 1f); //parameterise time
+    }
 
-        if(!RocketThrust.isPlaying){
-            RocketThrust.Play();
-            } 
+    private void StartDeathSequence(){
+        print("Hit Something Deadly");
+        CurrentStatus = State.Dying;
+        CurrentAudioSound.Stop();
+        CurrentAudioSound.PlayOneShot(DeathClip);
+        DeathAnimation.Play();
+        Invoke("LoadFirstLevel", 1f); //parameterise time
+    }
+
+    //Level Swapping
+    private void LoadNextLevel(){
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstLevel(){
+        SceneManager.LoadScene(0);
+    }
+
+    private void ThrustInputResponse(){
+        if(Input.GetKey(KeyCode.Space)){
+            ApplyThrust();
         } else {
-            RocketThrust.Stop();
+            CurrentAudioSound.Stop();
+            MainEngineAnimation.Stop();
+        }
+    }
+
+    //Movement Controls
+    private void ApplyThrust(){
+        rgbody.AddRelativeForce(Vector3.up * mainThrust);
+        print("Thrusting");
+
+        if(!CurrentAudioSound.isPlaying){
+            CurrentAudioSound.PlayOneShot(MainEngine);
+        }
+        if (!MainEngineAnimation.isPlaying) {
+            MainEngineAnimation.Play();
         }
     }
     
-    private void Rotate(){
+    private void RotateInputResponse(){
 
         rgbody.freezeRotation = true; //take manual control of rotation
         float rotationThisFrame = rotationSpeed * Time.deltaTime;
