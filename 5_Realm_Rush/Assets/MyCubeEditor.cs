3f52d5ce0777c979ba5d8b7eeb87881bd7317409
RevenantBane <kenbob.21@gmail.com>
2020-10-25 09:42:25 -0700

    -Started Realm Rush Project -Designed a 10x10x10 cube prefab made out of quads -Created a script editor to snap the cube(s) by 10 -Added a text mesh layer on top of the cube prefeb -Changed the text in the text mesh from the script to correspond to exact coordinates

---
+++ 5_Realm_Rush/Assets/MyCubeEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class MyCubeEditor : MonoBehaviour {
+    
+    [SerializeField] [Range(1f, 20f)] float GridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+
+        Vector3 SnapPos;
+
+        // Round the x position to 1 and multiply by 10 so it snaps through the script instead of the editor
+        //Snap everything to grid by 10
+        SnapPos.x = Mathf.RoundToInt(transform.position.x / GridSize) * GridSize;
+        SnapPos.z = Mathf.RoundToInt(transform.position.z / GridSize) * GridSize;
+        transform.position = new Vector3(SnapPos.x, 0f, SnapPos.z);
+
+        //Grab the text component in child of cube and set it to positions "x,y"
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = SnapPos.x / GridSize + "," + SnapPos.z / GridSize;
+    }
+}
